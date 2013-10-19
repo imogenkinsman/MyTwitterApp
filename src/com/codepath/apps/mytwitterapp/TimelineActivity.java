13fd6e3545ea2e5a3798c9ae324bb89845609e09
Jade McGough <jade@thezets.com>
2013-10-19 04:40:34 -0700

    initial commit

---
+++ src/com/codepath/apps/mytwitterapp/TimelineActivity.java
@@ -0,0 +1,33 @@
+package com.codepath.apps.mytwitterapp;
+
+import org.json.JSONArray;
+
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class TimelineActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_timeline);
+		
+		MyTwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				Log.d("DEBUG", jsonTweets.toString());
+			}
+		});
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.timeline, menu);
+		return true;
+	}
+
+}
