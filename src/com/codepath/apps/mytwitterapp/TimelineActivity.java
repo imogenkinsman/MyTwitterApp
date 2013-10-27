ba19cb58b6204343625eedfe7211b3296fff4e67
Jade McGough <jade@thezets.com>
2013-10-27 03:44:18 -0700

    added pull-to-refresh

--- src/com/codepath/apps/mytwitterapp/TimelineActivity.java
+++ src/com/codepath/apps/mytwitterapp/TimelineActivity.java
@@ -11,17 +11,19 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.codepath.apps.mytwitterapp.models.Tweet;
 import com.loopj.android.http.JsonHttpResponseHandler;
 
+import eu.erikw.PullToRefreshListView;
+import eu.erikw.PullToRefreshListView.OnRefreshListener;
+
 public class TimelineActivity extends Activity {
 
 	private static final int REQUEST_CODE = 0;
 	TweetsAdapter twtAdapter;
-	ListView lvTweets;
+	PullToRefreshListView lvTweets;
 	ArrayList<Tweet> tweets;
 	
 	@Override
@@ -29,34 +31,8 @@ public class TimelineActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_timeline);
 		setupViews();
-				
-		MyTwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
-			@Override
-			public void onSuccess(JSONArray jsonTweets) {
-				tweets = Tweet.fromJson(jsonTweets);
-					
-				twtAdapter = new TweetsAdapter(getBaseContext(), tweets);
-				lvTweets.setAdapter(twtAdapter);
-			}
-			
-			@Override
-			public void onFailure(Throwable e, JSONObject error) {
-				Log.e("ERROR", e.toString());
-				Toast.makeText(TimelineActivity.this, "Unable To Access Tweets", Toast.LENGTH_SHORT).show();
-			}
-		});
-		
-		lvTweets.setOnScrollListener(new EndlessScrollListener() {
-			@Override
-			public void onLoadMore(int page, int totalItemsCount) {
-				MyTwitterApp.getRestClient().getOldTimeLine(tweets.get(totalItemsCount-1).getTweetId(), new JsonHttpResponseHandler() {
-					@Override
-					public void onSuccess(JSONArray jsonTweets) {	
-						twtAdapter.addAll(Tweet.fromJson(jsonTweets));
-					}
-				});
-			}
-		});
+		setListeners();
+		fetchTimelineAsync(true);
 	}
 
 	@Override
@@ -67,7 +43,56 @@ public class TimelineActivity extends Activity {
 	}
 	
 	public void setupViews() {
-		lvTweets = (ListView) findViewById(R.id.lvTweets);
+		lvTweets = (PullToRefreshListView) findViewById(R.id.lvTweets);
+	}
+	
+	public void setListeners() {
+		
+		lvTweets.setOnScrollListener(new EndlessScrollListener() {
+			@Override
+			public void onLoadMore(int page, int totalItemsCount) {
+				// need to do totalItemsCount - 2 because the PullToRefresh adds another hidden item to the list
+				MyTwitterApp.getRestClient().getOldTimeLine(tweets.get(totalItemsCount-2).getTweetId(), new JsonHttpResponseHandler() {
+					@Override
+					public void onSuccess(JSONArray jsonTweets) {	
+						twtAdapter.addAll(Tweet.fromJson(jsonTweets));
+					}
+				});
+			}
+		});
+		
+		lvTweets.setOnRefreshListener(new OnRefreshListener() {
+			
+			@Override
+			public void onRefresh() {
+				fetchTimelineAsync(false);
+			}
+		});
+	}
+	
+	private void fetchTimelineAsync(final boolean firstLoad) {
+		MyTwitterApp.getRestClient().getHomeTimeline(new JsonHttpResponseHandler() {
+			@Override
+			public void onSuccess(JSONArray jsonTweets) {
+				tweets = Tweet.fromJson(jsonTweets);
+					
+				
+				if (firstLoad) {
+					twtAdapter = new TweetsAdapter(getBaseContext(), tweets);
+					lvTweets.setAdapter(twtAdapter);
+				} else {
+					twtAdapter.clear();
+					twtAdapter.addAll(tweets);
+					lvTweets.onRefreshComplete();
+				}
+			}
+			
+			@Override
+			public void onFailure(Throwable e, JSONObject error) {
+				Log.e("ERROR", e.toString());
+				Toast.makeText(TimelineActivity.this, "Unable To Access Tweets", Toast.LENGTH_SHORT).show();
+			}
+		});
 	}
 	
 	public void onComposeAction(MenuItem mi) {
