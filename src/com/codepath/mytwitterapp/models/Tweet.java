346445d7fd2b8fb8a8c6067196ee1a03fb65f221
Jade McGough <jade@thezets.com>
2013-10-27 11:20:43 -0700

    adding Tweet db query method

--- src/com/codepath/mytwitterapp/models/Tweet.java
+++ src/com/codepath/mytwitterapp/models/Tweet.java
@@ -10,6 +10,7 @@ import org.json.JSONObject;
 import com.activeandroid.Model;
 import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
 
 @Table(name = "Tweet")
 public class Tweet extends Model implements Serializable {
@@ -31,63 +32,65 @@ public class Tweet extends Model implements Serializable {
 		super();
 	}
   
-//Add a constructor that creates an object from the JSON response
- public static Tweet fromJson(JSONObject object){
-	 Tweet tweet = new Tweet();
+	//Add a constructor that creates an object from the JSON response
+	public static Tweet fromJson(JSONObject object){
+		Tweet tweet = new Tweet();
 
-   try {
-     tweet.tweetId = object.getLong("id");
-     tweet.timestamp = object.getString("created_at");
-     tweet.body = object.getString("text");
-     tweet.user = User.fromJson(object.getJSONObject("user"));
-          
-     // add user to db if doesn't exist
-//     if (User.getFromId(this.userId) == null) {
-//    	 User u = new User(object.getJSONObject("user"));
-//    	 u.save();
-//     }
-     
-   } catch (JSONException e) {
-     e.printStackTrace();
-     return null;
-   }
-   return tweet;
- }
+		try {
+			tweet.tweetId = object.getLong("id");
+			tweet.timestamp = object.getString("created_at");
+			tweet.body = object.getString("text");
+			tweet.user = User.fromJson(object.getJSONObject("user"));    
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return tweet;
+	}
+	
+	public static ArrayList<Tweet> fromJson(JSONArray jsonArray) {
+		ArrayList<Tweet> tweets = new ArrayList<Tweet>(jsonArray.length());
 
- public static ArrayList<Tweet> fromJson(JSONArray jsonArray) {
-   ArrayList<Tweet> tweets = new ArrayList<Tweet>(jsonArray.length());
+		for (int i=0; i < jsonArray.length(); i++) {
+			JSONObject tweetJson = null;
+			try {
+				tweetJson = jsonArray.getJSONObject(i);
+			} catch (Exception e) {
+				e.printStackTrace();
+				continue;
+			}
 
-   for (int i=0; i < jsonArray.length(); i++) {
-       JSONObject tweetJson = null;
-       try {
-           tweetJson = jsonArray.getJSONObject(i);
-       } catch (Exception e) {
-           e.printStackTrace();
-           continue;
-       }
-
-       Tweet tweet = Tweet.fromJson(tweetJson);
-       tweet.save();
-       tweets.add(tweet);
-   }
-
-   return tweets;
- }
- 
- public User getUser() {
-	 return user;
- }
- 
- public String getBody() {
-	 return body;
- }
- 
- public String getTimeStamp() {
-	 return timestamp;
- }
- 
- public long getTweetId() {
-	 return tweetId;
- }
- 
+			Tweet tweet = Tweet.fromJson(tweetJson);
+			tweet.save();
+			tweets.add(tweet);
+		}
+	
+		return tweets;
+	}
+	 
+	public User getUser() {
+		return user;
+	}
+	 
+	public String getBody() {
+		return body;
+	}
+	 
+	public String getTimeStamp() {
+		return timestamp;
+	}
+	
+	public long getTweetId() {
+		return tweetId;
+	}
+	 
+	/**
+	 * This method returns the 25 most recent Tweets from the database. It should be used to display some data if a user opens the app while offline.
+	 * @return An ArrayList of up to 25 Tweets.
+	 */
+	public static ArrayList<Tweet> getTweetsFromDb() {
+		return new Select().from(Tweet.class).orderBy("timestamp DESC").limit("25").execute();
+	}
+	 
 }
